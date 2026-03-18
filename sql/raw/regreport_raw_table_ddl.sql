19e5aa308543113864459e236b6de6e0d1bf2051
gaurabdawn-dev <gaurab.comm@gmail.com>
2026-03-18 22:43:07 +0530

    Initial commit - regulatory reporting project

---
+++ sql/raw/regreport_raw_table_ddl.sql
@@ -0,0 +1,56 @@
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.transactions_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/trans.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.account_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/account.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.card_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/card.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.client_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/client.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.disp_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/disp.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.district_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/district.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.loan_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/loan.csv'],
+  skip_leading_rows = 1
+);
+
+CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.order_ext`
+OPTIONS (
+  format = 'CSV',
+  uris = ['gs://source_layer/order.csv'],
+  skip_leading_rows = 1
+);
