d2eba07186704be7bae826531f4c8fada5f866c9
gaurabdawn-dev <gaurab.comm@gmail.com>
2026-03-22 10:30:48 +0530

    external table path changes

--- sql/raw/regreport_raw_table_ddl.sql
+++ sql/raw/regreport_raw_table_ddl.sql
@@ -2,55 +2,55 @@
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.transactions_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/trans.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/trans.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.account_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/account.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/account.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.card_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/card.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/card.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.client_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/client.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/client.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.disp_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/disp.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/disp.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.district_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/district.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/district.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.loan_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/loan.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/loan.csv'],
   skip_leading_rows = 1
 );
 
 CREATE OR REPLACE EXTERNAL TABLE `regreport_raw.order_ext`
 OPTIONS (
   format = 'CSV',
-  uris = ['gs://source_layer/order.csv'],
+  uris = ['gs://reg-reporting-490417-dev-raw/source_layer/order.csv'],
   skip_leading_rows = 1
 );
